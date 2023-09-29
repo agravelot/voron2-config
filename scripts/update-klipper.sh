42358ab9a841815efebb36f901d98d2c48559d90
Antoine GRAVELOT <antoine.gravelot@hotmail.fr>
2023-09-29 22:14:24 +0200

    Autocommit from 2023-09-29 22:14:24
    Klipper on commit: 21b7842
    
    Moonraker on commit: ee62d07
    
    Mainsail version: v2.7.1

--- scripts/update-klipper.sh
+++ scripts/update-klipper.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-PWD="$PWD"
+DIR="$(pwd)"
 
 
 sudo service klipper stop
@@ -8,42 +8,42 @@ cd ~/klipper
 #git pull
 
 # Octopus
-make clean KCONFIG_CONFIG=$PWD/octopus.config
-#make menuconfig KCONFIG_CONFIG=$PWD/octopus.config
-make KCONFIG_CONFIG=$PWD/octopus.config
+echo $DIR/octopus.config
+make clean KCONFIG_CONFIG=$DIR/octopus.config
+make menuconfig KCONFIG_CONFIG=$DIR/octopus.config
+make KCONFIG_CONFIG=$DIR/octopus.config -j$(nproc)
 read -p "Octopus firmware built, please check above for any errors. Press [Enter] to continue flashing, or [Ctrl+C] to abort"
 
-#make KCONFIG_CONFIG=config.octopus flash FLASH_DEVICE=/dev/serial/by-id/YourMCUIDHere
-#./scripts/flash-sdcard.sh /dev/ttyAMA0 fysetc-spider-v1
+#make KCONFIG_CONFIG=config.octopus flash FLASH_DEVICE=/dev/serial/by-id/usb-Klipper_stm32f446xx_280020001551313133353932-if00
 read -p "Octopus firmware flashed, please check above for any errors. Press [Enter] to continue, or [Ctrl+C] to abort"
 
 
 # Rpi
-make clean KCONFIG_CONFIG=$PWD/config.rpi
-#make menuconfig KCONFIG_CONFIG=$PWD/config.rpi
+make clean KCONFIG_CONFIG=$DIR/rpi.config
+make menuconfig KCONFIG_CONFIG=$DIR/rpi.config
 
-make KCONFIG_CONFIG=$PWD/config.rpi
+make KCONFIG_CONFIG=$DIR/rpi.config -j$(nproc)
 read -p "RPi firmware built, please check above for any errors. Press [Enter] to continue flashing, or [Ctrl+C] to abort"
-#make flash KCONFIG_CONFIG=$PWD/config.rpi
+make flash KCONFIG_CONFIG=$DIR/rpi.config
 read -p "RPi firmware flashed, please check above for any errors. Press [Enter] to continue, or [Ctrl+C] to abort"
 
 # SB2240
 
-#make clean KCONFIG_CONFIG=$PWD/sb2240.config
-#make menuconfig KCONFIG_CONFIG=$PWD/sb2240.config
-#make KCONFIG_CONFIG=$PWD/sb2240.config
-#read -p "SB2240 firmware built, please check above for any errors. Press [Enter] to continue flashing, or [Ctrl+C] to abort"
+make clean KCONFIG_CONFIG=$DIR/sb2240.config
+make menuconfig KCONFIG_CONFIG=$DIR/sb2240.config
+make KCONFIG_CONFIG=$DIR/sb2240.config -j$(nproc)
+read -p "SB2240 firmware built, please check above for any errors. Press [Enter] to continue flashing, or [Ctrl+C] to abort"
 
-#python3 ~/katapult/scripts/flash_can.py -i can0 -u YourCANBUSUUIDHere -r
-#python3 ~/katapult/scripts/flash_can.py -i can0 -u YourCANBUSUUIDHere -f ~/klipper/out/klipper.bin
-#read -p "SB2240 firmware flashed, please check above for any errors. Press [Enter] to continue, or [Ctrl+C] to abort"
+python3 ~/katapult/scripts/flash_can.py -i can0 -u de2f42d3cb5a -r
+python3 ~/katapult/scripts/flash_can.py -i can0 -u de2f42d3cb5a -f ~/klipper/out/klipper.bin
+read -p "SB2240 firmware flashed, please check above for any errors. Press [Enter] to continue, or [Ctrl+C] to abort"
 
 
 # sht36 v2
 
-#make clean KCONFIG_CONFIG=$PWD/sh36v2.config
-#make menuconfig KCONFIG_CONFIG=$PWD/sht36v2.config
-#make KCONFIG_CONFIG=$PWD/sht36v2.config
+#make clean KCONFIG_CONFIG=$DIR/sh36v2.config
+#make menuconfig KCONFIG_CONFIG=$DIR/sht36v2.config
+#make KCONFIG_CONFIG=$DIR/sht36v2.config -j$(nproc)
 #read -p "sht36v2 firmware built, please check above for any errors. Press [Enter] to continue flashing, or [Ctrl+C] to abort"
 
 #python3 ~/katapult/scripts/flash_can.py -i can0 -u YourCANBUSUUIDHere -r
