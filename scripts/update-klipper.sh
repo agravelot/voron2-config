6a1d596bed06d8b8292f310ffa99eff93bf0fff5
Antoine GRAVELOT <antoine.gravelot@hotmail.fr>
2023-10-07 06:00:03 +0200

    Autocommit from 2023-10-07 06:00:03
    Klipper on commit: 83ef0e1
    
    Moonraker on commit: c50e61a
    
    Mainsail version: v2.7.1

--- scripts/update-klipper.sh
+++ scripts/update-klipper.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+
 DIR="$(pwd)"
 
 
@@ -29,25 +30,25 @@ read -p "RPi firmware flashed, please check above for any errors. Press [Enter] 
 
 # SB2240
 
-make clean KCONFIG_CONFIG=$DIR/sb2240.config
-make menuconfig KCONFIG_CONFIG=$DIR/sb2240.config
-make KCONFIG_CONFIG=$DIR/sb2240.config -j$(nproc)
-read -p "SB2240 firmware built, please check above for any errors. Press [Enter] to continue flashing, or [Ctrl+C] to abort"
+#make clean KCONFIG_CONFIG=$DIR/sb2240.config
+#make menuconfig KCONFIG_CONFIG=$DIR/sb2240.config
+#make KCONFIG_CONFIG=$DIR/sb2240.config -j$(nproc)
+#read -p "SB2240 firmware built, please check above for any errors. Press [Enter] to continue flashing, or [Ctrl+C] to abort"
 
-python3 ~/katapult/scripts/flash_can.py -i can0 -u de2f42d3cb5a -r
-python3 ~/katapult/scripts/flash_can.py -i can0 -u de2f42d3cb5a -f ~/klipper/out/klipper.bin
-read -p "SB2240 firmware flashed, please check above for any errors. Press [Enter] to continue, or [Ctrl+C] to abort"
+#python3 ~/katapult/scripts/flash_can.py -i can0 -u de2f42d3cb5a -r
+#python3 ~/katapult/scripts/flash_can.py -i can0 -u de2f42d3cb5a -f ~/klipper/out/klipper.bin
+#read -p "SB2240 firmware flashed, please check above for any errors. Press [Enter] to continue, or [Ctrl+C] to abort"
 
 
 # sht36 v2
 
-#make clean KCONFIG_CONFIG=$DIR/sh36v2.config
-#make menuconfig KCONFIG_CONFIG=$DIR/sht36v2.config
-#make KCONFIG_CONFIG=$DIR/sht36v2.config -j$(nproc)
-#read -p "sht36v2 firmware built, please check above for any errors. Press [Enter] to continue flashing, or [Ctrl+C] to abort"
+make clean KCONFIG_CONFIG=$DIR/sh36v2.config
+make menuconfig KCONFIG_CONFIG=$DIR/sht36v2.config
+make KCONFIG_CONFIG=$DIR/sht36v2.config -j$(nproc)
+read -p "sht36v2 firmware built, please check above for any errors. Press [Enter] to continue flashing, or [Ctrl+C] to abort"
 
-#python3 ~/katapult/scripts/flash_can.py -i can0 -u YourCANBUSUUIDHere -r
-#python3 ~/katapult/scripts/flash_can.py -i can0 -u YourCANBUSUUIDHere -f ~/klipper/out/klipper.bin
-#read -p "sht36v2 firmware flashed, please check above for any errors. Press [Enter] to continue, or [Ctrl+C] to abort"
+python3 ~/katapult/scripts/flash_can.py -i can0 -u 05888914ca52 -r
+python3 ~/katapult/scripts/flash_can.py -i can0 -u 05888914ca52 -f ~/klipper/out/klipper.bin
+read -p "sht36v2 firmware flashed, please check above for any errors. Press [Enter] to continue, or [Ctrl+C] to abort"
 
 sudo service klipper start
