da479c239028a1be34d3004e46cff031ebd1090c
Antoine GRAVELOT <antoine.gravelot@hotmail.fr>
2023-10-14 02:48:12 +0200

    Autocommit from 2023-10-14 02:48:12
    Klipper on commit: 83ef0e1
    
    Moonraker on commit: 9785387
    
    Mainsail version: v2.8.0

--- scripts/update-klipper.sh
+++ scripts/update-klipper.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+set -xe
 
 DIR="$(pwd)"
 
@@ -8,25 +9,30 @@ sudo service klipper stop
 cd ~/klipper
 #git pull
 
+# reboot in bootloader
+cd ~/klipper/scripts
+python3 -c 'import flash_usb as u; u.enter_bootloader("/dev/serial/by-id/usb-Klipper_stm32f446xx_280020001551313133353932-if00")'
+cd -
+
 # Octopus
 echo $DIR/octopus.config
 make clean KCONFIG_CONFIG=$DIR/octopus.config
-make menuconfig KCONFIG_CONFIG=$DIR/octopus.config
+#make menuconfig KCONFIG_CONFIG=$DIR/octopus.config
 make KCONFIG_CONFIG=$DIR/octopus.config -j$(nproc)
-read -p "Octopus firmware built, please check above for any errors. Press [Enter] to continue flashing, or [Ctrl+C] to abort"
+#read -p "Octopus firmware built, please check above for any errors. Press [Enter] to continue flashing, or [Ctrl+C] to abort"
 
-#make KCONFIG_CONFIG=config.octopus flash FLASH_DEVICE=/dev/serial/by-id/usb-Klipper_stm32f446xx_280020001551313133353932-if00
-read -p "Octopus firmware flashed, please check above for any errors. Press [Enter] to continue, or [Ctrl+C] to abort"
+make KCONFIG_CONFIG=$DIR/octopus.config flash FLASH_DEVICE=/dev/serial/by-id/usb-katapult_stm32f446xx_280020001551313133353932-if00
+#read -p "Octopus firmware flashed, please check above for any errors. Press [Enter] to continue, or [Ctrl+C] to abort"
 
 
 # Rpi
 make clean KCONFIG_CONFIG=$DIR/rpi.config
-make menuconfig KCONFIG_CONFIG=$DIR/rpi.config
+#make menuconfig KCONFIG_CONFIG=$DIR/rpi.config
 
 make KCONFIG_CONFIG=$DIR/rpi.config -j$(nproc)
-read -p "RPi firmware built, please check above for any errors. Press [Enter] to continue flashing, or [Ctrl+C] to abort"
+#read -p "RPi firmware built, please check above for any errors. Press [Enter] to continue flashing, or [Ctrl+C] to abort"
 make flash KCONFIG_CONFIG=$DIR/rpi.config
-read -p "RPi firmware flashed, please check above for any errors. Press [Enter] to continue, or [Ctrl+C] to abort"
+#read -p "RPi firmware flashed, please check above for any errors. Press [Enter] to continue, or [Ctrl+C] to abort"
 
 # SB2240
 
@@ -43,12 +49,12 @@ read -p "RPi firmware flashed, please check above for any errors. Press [Enter] 
 # sht36 v2
 
 make clean KCONFIG_CONFIG=$DIR/sh36v2.config
-make menuconfig KCONFIG_CONFIG=$DIR/sht36v2.config
+#make menuconfig KCONFIG_CONFIG=$DIR/sht36v2.config
 make KCONFIG_CONFIG=$DIR/sht36v2.config -j$(nproc)
-read -p "sht36v2 firmware built, please check above for any errors. Press [Enter] to continue flashing, or [Ctrl+C] to abort"
+#read -p "sht36v2 firmware built, please check above for any errors. Press [Enter] to continue flashing, or [Ctrl+C] to abort"
 
 python3 ~/katapult/scripts/flash_can.py -i can0 -u 05888914ca52 -r
 python3 ~/katapult/scripts/flash_can.py -i can0 -u 05888914ca52 -f ~/klipper/out/klipper.bin
-read -p "sht36v2 firmware flashed, please check above for any errors. Press [Enter] to continue, or [Ctrl+C] to abort"
+#read -p "sht36v2 firmware flashed, please check above for any errors. Press [Enter] to continue, or [Ctrl+C] to abort"
 
 sudo service klipper start
