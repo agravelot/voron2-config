1299ca880c857cdf4341d5ddde6d00f37f112f27
Antoine GRAVELOT <antoine.gravelot@hotmail.fr>
2024-12-06 00:38:43 +0100

    Autocommit from 2024-12-06 00:38:43
    Klipper on commit: 7f89668
    
    Moonraker on commit: ccfe32f
    
    Mainsail version: v2.13.0

--- scripts/update-klipper.sh
+++ scripts/update-klipper.sh
@@ -27,18 +27,18 @@ make KCONFIG_CONFIG=$DIR/octopus.config flash FLASH_DEVICE=/dev/serial/by-id/usb
 # LIS2DW
 
 # reboot in bootloader
-cd ~/klipper/scripts
-python3 -c 'import flash_usb as u; u.enter_bootloader("/dev/serial/by-id/usb-Klipper_rp2040_btt_acc-if00")'
-cd -
+#cd ~/klipper/scripts
+#python3 -c 'import flash_usb as u; u.enter_bootloader("/dev/serial/by-id/usb-Klipper_rp2040_btt_acc-if00")'
+#cd -
 
-echo $DIR/lis2dw.config
-make clean KCONFIG_CONFIG=$DIR/lis2dw.config
+#echo $DIR/lis2dw.config
+#make clean KCONFIG_CONFIG=$DIR/lis2dw.config
 #make menuconfig KCONFIG_CONFIG=$DIR/lis2dw.config
-make KCONFIG_CONFIG=$DIR/lis2dw.config -j$(nproc)
-#read -p "LIS2DW firmware built, please check above for any errors. Press [Enter] to continue flashing, or [Ctrl+C] to abort"
+#make KCONFIG_CONFIG=$DIR/lis2dw.config -j$(nproc)
+##read -p "LIS2DW firmware built, please check above for any errors. Press [Enter] to continue flashing, or [Ctrl+C] to abort"
 
-make KCONFIG_CONFIG=$DIR/lis2dw.config flash FLASH_DEVICE=2e8a:0003
-#read -p "LIS2DW firmware flashed, please check above for any errors. Press [Enter] to continue, or [Ctrl+C] to abort"
+#make KCONFIG_CONFIG=$DIR/lis2dw.config flash FLASH_DEVICE=2e8a:0003
+##read -p "LIS2DW firmware flashed, please check above for any errors. Press [Enter] to continue, or [Ctrl+C] to abort"
 
 
 # Rpi
