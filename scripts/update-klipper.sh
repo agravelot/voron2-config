65f08190c89bcf646d783ce4a21963992ef3275e
Antoine GRAVELOT <antoine.gravelot@hotmail.fr>
2025-01-03 12:01:39 +0100

    Autocommit from 2025-01-03 12:01:39
    Klipper on commit: 55336a1
    
    Moonraker on commit: a4604e3
    
    Mainsail version: v2.13.2

--- scripts/update-klipper.sh
+++ scripts/update-klipper.sh
@@ -1,76 +1,93 @@
 #!/bin/bash
 
-set -xe
+set -xe  # Exit on error and print commands
 
 DIR="$(pwd)"
+KLIPPER_DIR=~/klipper
+KLIPPER_SCRIPTS="$KLIPPER_DIR/scripts"
+KATAPULT_SCRIPTS=~/katapult/scripts
 
+RUN_MENUCONFIG=false  # Default: do not run menuconfig
 
-sudo service klipper stop
-cd ~/klipper
-#git pull
+# Check for --menuconfig argument
+if [[ "$1" == "--menuconfig" ]]; then
+    RUN_MENUCONFIG=true
+fi
 
+stop_klipper() {
+    sudo service klipper stop
+}
+
+start_klipper() {
+    sudo service klipper start
+}
+
+build() {
+    local config_file="$1"
+
+    echo "Building firmware for $config_file"
+    cd $KLIPPER_DIR
+    make clean KCONFIG_CONFIG="$config_file"
+    
+    if $RUN_MENUCONFIG; then
+        make menuconfig KCONFIG_CONFIG="$config_file"
+    fi
+
+    make KCONFIG_CONFIG="$config_file" -j"$(nproc)"
+}
+
+flash_usb() {
+    local config_file="$1"
+    local flash_device="$2"
+
+    echo "Flashing firmware for $config_file"
+    cd $KLIPPER_DIR
+   
+    make KCONFIG_CONFIG="$config_file" flash FLASH_DEVICE="$flash_device"
+}
+
+flash_can() {
+    local config_file="$1"
+    local unique_id="$2"
+
+    echo "Flashing firmware for $config_file via CAN bus"
+    cd $KLIPPER_DIR
+
+    python3 "$KATAPULT_SCRIPTS/flash_can.py" -i can0 -u "$unique_id" -r
+    python3 "$KATAPULT_SCRIPTS/flash_can.py" -i can0 -u "$unique_id" -f "$KLIPPER_DIR/out/klipper.bin"
+}
+
+enter_bootloader() {
+    local device_path="$1"
+    cd $KLIPPER_SCRIPTS
+
+    echo "Rebooting device into bootloader: $device_path"
+    python3 -c "import flash_usb as u; u.enter_bootloader(\"$device_path\")"
+    cd -
+}
+
+# Main script execution
+stop_klipper
 
 # Octopus
-# reboot in bootloader
-cd ~/klipper/scripts
-python3 -c 'import flash_usb as u; u.enter_bootloader("/dev/serial/by-id/usb-Klipper_stm32f446xx_280020001551313133353932-if00")'
-cd -
-echo $DIR/octopus.config
-make clean KCONFIG_CONFIG=$DIR/octopus.config
-#make menuconfig KCONFIG_CONFIG=$DIR/octopus.config
-make KCONFIG_CONFIG=$DIR/octopus.config -j$(nproc)
-#read -p "Octopus firmware built, please check above for any errors. Press [Enter] to continue flashing, or [Ctrl+C] to abort"
+enter_bootloader "/dev/serial/by-id/usb-Klipper_stm32f446xx_280020001551313133353932-if00"
+build "$DIR/octopus.config"
+flash_usb "$DIR/octopus.config" "/dev/serial/by-id/usb-katapult_stm32f446xx_280020001551313133353932-if00"
 
-make KCONFIG_CONFIG=$DIR/octopus.config flash FLASH_DEVICE=/dev/serial/by-id/usb-katapult_stm32f446xx_280020001551313133353932-if00
-#read -p "Octopus firmware flashed, please check above for any errors. Press [Enter] to continue, or [Ctrl+C] to abort"
+# LIS2DW (uncomment to enable)
+# enter_bootloader "/dev/serial/by-id/usb-Klipper_rp2040_btt_acc-if00"
+# build_and_flash_usb "$DIR/lis2dw.config" "2e8a:0003"
 
-# LIS2DW
+# RPi
+build "$DIR/rpi.config"
+flash_usb "$DIR/rpi.config"
 
-# reboot in bootloader
-#cd ~/klipper/scripts
-#python3 -c 'import flash_usb as u; u.enter_bootloader("/dev/serial/by-id/usb-Klipper_rp2040_btt_acc-if00")'
-#cd -
+# SB2240 (uncomment to enable)
+# build "$DIR/sb2240.config" "de2f42d3cb5a"
+# flash_can "$DIR/sb2240.config" "de2f42d3cb5a"
 
-#echo $DIR/lis2dw.config
-#make clean KCONFIG_CONFIG=$DIR/lis2dw.config
-#make menuconfig KCONFIG_CONFIG=$DIR/lis2dw.config
-#make KCONFIG_CONFIG=$DIR/lis2dw.config -j$(nproc)
-##read -p "LIS2DW firmware built, please check above for any errors. Press [Enter] to continue flashing, or [Ctrl+C] to abort"
+# sht36v2
+build "$DIR/sht36v2.config" "05888914ca52"
+flash_can "$DIR/sht36v2.config" "05888914ca52"
 
-#make KCONFIG_CONFIG=$DIR/lis2dw.config flash FLASH_DEVICE=2e8a:0003
-##read -p "LIS2DW firmware flashed, please check above for any errors. Press [Enter] to continue, or [Ctrl+C] to abort"
-
-
-# Rpi
-make clean KCONFIG_CONFIG=$DIR/rpi.config
-#make menuconfig KCONFIG_CONFIG=$DIR/rpi.config
-
-make KCONFIG_CONFIG=$DIR/rpi.config -j$(nproc)
-#read -p "RPi firmware built, please check above for any errors. Press [Enter] to continue flashing, or [Ctrl+C] to abort"
-make flash KCONFIG_CONFIG=$DIR/rpi.config
-#read -p "RPi firmware flashed, please check above for any errors. Press [Enter] to continue, or [Ctrl+C] to abort"
-
-# SB2240
-
-#make clean KCONFIG_CONFIG=$DIR/sb2240.config
-#make menuconfig KCONFIG_CONFIG=$DIR/sb2240.config
-#make KCONFIG_CONFIG=$DIR/sb2240.config -j$(nproc)
-#read -p "SB2240 firmware built, please check above for any errors. Press [Enter] to continue flashing, or [Ctrl+C] to abort"
-
-#python3 ~/katapult/scripts/flash_can.py -i can0 -u de2f42d3cb5a -r
-#python3 ~/katapult/scripts/flash_can.py -i can0 -u de2f42d3cb5a -f ~/klipper/out/klipper.bin
-#read -p "SB2240 firmware flashed, please check above for any errors. Press [Enter] to continue, or [Ctrl+C] to abort"
-
-
-# sht36 v2
-
-make clean KCONFIG_CONFIG=$DIR/sh36v2.config
-#make menuconfig KCONFIG_CONFIG=$DIR/sht36v2.config
-make KCONFIG_CONFIG=$DIR/sht36v2.config -j$(nproc)
-#read -p "sht36v2 firmware built, please check above for any errors. Press [Enter] to continue flashing, or [Ctrl+C] to abort"
-
-python3 ~/katapult/scripts/flash_can.py -i can0 -u 05888914ca52 -r
-python3 ~/katapult/scripts/flash_can.py -i can0 -u 05888914ca52 -f ~/klipper/out/klipper.bin
-#read -p "sht36v2 firmware flashed, please check above for any errors. Press [Enter] to continue, or [Ctrl+C] to abort"
-
-sudo service klipper start
+start_klipper
